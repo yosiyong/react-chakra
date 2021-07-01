dcfd085106f00adbb3ea259f7cf34408da92c728
YongHun Lee <yosiyong@gmail.com>
2021-07-02 00:13:29 +0900

    ヘッダー作成

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 
 import { Login } from "../components/pages/Login";
 import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
 import { HomeRoutes } from "./HomeRoutes";
 
 export const Router: VFC = memo(() => {
@@ -21,7 +22,7 @@ export const Router: VFC = memo(() => {
                 exact={route.exact}
                 path={`${url}${route.path}`}
               >
-                {route.children}
+                <HeaderLayout>{route.children}</HeaderLayout>
               </Route>
             ))}
           </Switch>
