5346f3511fc9ceea522f3a256817b3439da47996
YongHun Lee <yosiyong@gmail.com>
2021-07-07 23:36:11 +0900

    ユーザー一覧

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,19 @@
 import { memo, VFC } from "react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページです。</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      {[...Array(10)].map(() => (
+        <WrapItem>
+          <UserCard
+            imageUrl="https://source.unsplash.com/random"
+            userName="yosiyong"
+            fullName="YongHun"
+          />
+        </WrapItem>
+      ))}
+    </Wrap>
+  );
 });
