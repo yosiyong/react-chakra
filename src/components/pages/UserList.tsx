b5771b61bd04670c3970e8002d19ddaade578bdd
YongHun Lee <yosiyong@gmail.com>
2021-07-08 23:09:27 +0900

    ユーザー一覧作成

---
+++ src/components/pages/UserList.tsx
@@ -0,0 +1,32 @@
+import { memo, useEffect, VFC } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
+
+export const UserList: VFC = memo(() => {
+  const { getUsers, loading, users } = useAllUsers();
+
+  useEffect(() => getUsers(), [getUsers]);
+
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }} justify="center">
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+  );
+});
